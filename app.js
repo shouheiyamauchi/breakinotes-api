458828064fdb5b3a2445d287b767a3dd8d0ec09b
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-12-16 22:03:25 +1100

    add moves controller

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+const MoveController = require('./move/MoveController');
+app.use('/moves', MoveController);
+
 module.exports = app;
---
+++ move/MoveController.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.user(bodyParer.urlencoded({ encoded: true }));
+const Move = require('./Move');
+
+router.post('/', (req, res) => {
+  Move.create({
+    name: req.body.name,
+    creationCategory: req.body.creationCategory,
+    moveCategory: req.body.moveCategory,
+    notes: req.body.notes
+  }, (err, move) => {
+    if (err) {
+      res.status(500).send("There was a problem adding the move to the database.");
+    } else {
+      res.status(200).send(move);
+    };
+  });
+});
+
+router.get('/', (req, res) => {
+  Move.find({}, (err, moves) => {
+    if (err) {
+      res.status(500).send("There was a problem finding the moves.");
+    } else {
+      res.status(200).send(moves);
+    };
+  });
+});
+
+// app.get('/notes/:id', (req, res) => {
+//   const id = req.params.id;
+//   const details = { '_id': new ObjectID(id) };
+//   db.collection('notes').findOne(details, (err, item) => {
+//     if (err) {
+//       res.send({'error':'An error has occurred'});
+//     } else {
+//       res.send(item);
+//     };
+//   });
+// });
+//
+// app.delete('/notes/:id', (req, res) => {
+//   const id = req.params.id;
+//   const details = { '_id': new ObjectID(id) };
+//   db.collection('notes').remove(details, (err, item) => {
+//     if (err) {
+//       res.send({'error':'An error has occurred'});
+//     } else {
+//       res.send('Note ' + id + ' deleted');
+//     };
+//   });
+// });
+//
+// app.put('/notes/:id', (req, res) => {
+//   const id = req.params.id;
+//   const details = { '_id': new ObjectID(id) };
+//   const note  = { 'text': req.body.body, 'title': req.body.title };
+//   db.collection('notes').update(details, note, (err, result) => {
+//     if (err) {
+//       res.send({'error': 'An error has occurred'});
+//     } else {
+//       res.send(note);
+//     };
+//   });
+// });
+
+module.exports = router;
