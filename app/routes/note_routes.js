30d07d2478621877c70e3aaeba4bb6d69c1137a9
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-12-16 15:13:40 +1100

    add get and update endpoints

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,4 +1,6 @@
-module.exports = function(app, db) {  
+var ObjectID = require('mongodb').ObjectID;
+
+module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
@@ -6,7 +8,32 @@ module.exports = function(app, db) {
         res.send({ 'error': 'An error has occured' });
       } else {
         res.send(result.ops[0]);
-      }
+      };
+    });
+  });
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(item);
+      };
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note  = { 'text': req.body.body, 'title': req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+        res.send({'error': 'An error has occurred'});
+      } else {
+        res.send(note);
+      };
     });
   });
 };
--- server.js
+++ server.js
@@ -1,20 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const db = require('./config/db')
 
 const app = express();
-
 const port = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-MongoClient.connect(db.url, (err, database) => {
-  if (err) return console.log(err);
-  const practiceDb = database.db('practice');
-  require('./app/routes')(app, practiceDb);
+MongoClient.connect(process.env.DB_URL, (err, dbConnection) => {
+  if (err) {
+    return console.log(err);
+  } else {
+    const database = dbConnection.db(process.env.DB_NAME);
 
-  app.listen(port, () => {
-    console.log('Server running on port: ' + port);
-  });
+    require('./app/routes')(app, database);
+
+    app.listen(port, () => {
+      console.log('Server running on port: ' + port);
+    });
+  };
 });
