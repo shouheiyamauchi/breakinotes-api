f52b7e42976b7285e04074efaa17060938edd573
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-12-16 22:05:27 +1100

    update server.js

--- server.js
+++ server.js
@@ -3,21 +3,6 @@ require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
-const MongoClient = require('mongodb').MongoClient;
-const bodyParser = require('body-parser');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-MongoClient.connect(process.env.DB_URL, (err, dbConnection) => {
-  if (err) {
-    return console.log(err);
-  } else {
-    const database = dbConnection.db(process.env.DB_NAME);
-
-    require('./app/routes')(app, database);
-
-    app.listen(port, () => {
-      console.log('Express server listening on port ' + port);
-    });
-  };
+const server = app.listen(port, () => {
+  console.log('Express server listening on port ' + port);
 });
