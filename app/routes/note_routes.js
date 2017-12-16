2ea2c3c5406f1bd07b2bcb9901d67ed9d0038377
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-12-16 15:19:05 +1100

    add delete endpoint

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -5,7 +5,7 @@ module.exports = function(app, db) {
     const note = { text: req.body.body, title: req.body.title };
     db.collection('notes').insert(note, (err, result) => {
       if (err) {
-        res.send({ 'error': 'An error has occured' });
+        res.send({'error':'An error has occured'});
       } else {
         res.send(result.ops[0]);
       };
@@ -17,9 +17,21 @@ module.exports = function(app, db) {
     const details = { '_id': new ObjectID(id) };
     db.collection('notes').findOne(details, (err, item) => {
       if (err) {
-        res.send({'error': 'An error has occurred'});
+        res.send({'error':'An error has occurred'});
       } else {
         res.send(item);
+      };
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').remove(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send('Note ' + id + ' deleted');
       };
     });
   });
