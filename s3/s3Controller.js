89c2d3275c01686ed9220d4531c220ef0b7f3780
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2018-01-05 08:20:52 +1100

    add s3 uploader

---
+++ s3/s3Controller.js
@@ -0,0 +1,41 @@
+const aws = require('aws-sdk');
+
+aws.config.region = 'ap-southeast-2';
+const S3_BUCKET = process.env.S3_BUCKET;
+
+module.exports = {
+  signedUrl: (req, res) => {
+    const s3 = new aws.S3();
+    const fileName = req.body.fileName;
+    const fileType = req.body.fileType;
+    const s3Params = {
+      Bucket: S3_BUCKET,
+      Key: fileName,
+      Expires: 60,
+      ContentType: fileType,
+      ACL: 'private'
+    };
+
+    s3.getSignedUrl('putObject', s3Params, (err, data) => {
+      if (err) res.status(500).send(err);
+      const returnData = {
+        signedRequest: data,
+        url: 'https://' + S3_BUCKET + '.s3.amazonaws.com/' + fileName
+      };
+      res.status(200).send(returnData);
+    });
+  },
+  delete: () => {
+    const s3 = new aws.S3();
+    const fileName = req.body.fileName;
+    const s3Params = {
+     Bucket: S3_BUCKET,
+     Key: fileName
+    };
+
+    s3.deleteObject(s3Params, (err, data) => {
+      if (err) res.status(500).send(err);
+      res.status(200).send(fileName + ' was deleted');
+    });
+  }
+};
