f94050007af05e95d99939f663c2e61febaef37a
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2018-01-14 20:25:52 +1100

    add helmet for security

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const db = require('./db');
 const moveRoutes = require('./move/moveRoutes');
 const s3Routes = require('./s3/s3Routes');
 
 const app = express();
 app.use(cors());
+app.use(helmet());
 app.use('/moves', moveRoutes);
 app.use('/s3', s3Routes);
 
